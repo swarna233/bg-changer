b6cfea4b677b1bf8f68a76ba31b273d7b8e42613
Swarna Gupta <gupta.swarna1997@gmail.com>
2023-12-30 16:59:43 +0530

    changed color to dark brown

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ function App() {
         bg-white px-3 py-2 rounded-3xl"
         >
           <button
-            onClick={() => setColor("green")}
+            onClick={() => setColor("brown")}
             className="outline-none px-4 py-1
           rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "green" }}
+            style={{ backgroundColor: "brown" }}
           >
-            Green
+            Dark Brown
           </button>
           <button
             onClick={() => setColor("blue")}
