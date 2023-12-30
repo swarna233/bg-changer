d8731b9cd19fd8c3634f6d9bae7d92d7fcc7a4a0
Swarna Gupta <gupta.swarna1997@gmail.com>
2023-12-30 16:56:46 +0530

    changed colour names to green

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ function App() {
         bg-white px-3 py-2 rounded-3xl"
         >
           <button
-            onClick={() => setColor("red")}
+            onClick={() => setColor("green")}
             className="outline-none px-4 py-1
           rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "brown" }}
+            style={{ backgroundColor: "green" }}
           >
-            Brown
+            Green
           </button>
           <button
             onClick={() => setColor("blue")}
